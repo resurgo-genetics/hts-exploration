1ed9ede6d350a46e683ad4a515858da6e6f91e03
Meyerlab <meyerlab@meyerlab.bc.edu>
2014-07-16 18:45:07 -0400

    figured out how to traverse the tree and add weights

--- src/hts_exploration/upgma.clj
+++ src/hts_exploration/upgma.clj
@@ -1,4 +1,5 @@
-(ns hts-exploration.upgma)
+(ns hts-exploration.upgma
+  (:require clojure.set))
 
 (def ^:dynamic *M*)
 (defn min-entry [m] (first (sort-by second m)))
@@ -34,12 +35,25 @@
                (into (pop queue) children)))
       ret)))
 
-(defn tree-traversal [weights tree]
+(defn tree-traversal [tree]
   (loop [t tree
-         q []]
-    (if (seq tree)
-      (recur (first t) (conj q tree))
-      (weights tree))))
+         stack []]
+    (let [x (first (pop t))]
+      (prn :pop x :stack stack :count (count (flatten x)))
+      (if (>= (count (flatten x)) 2)
+        (recur x
+               (conj stack (peek t)))
+        [t stack]))))
+
+(defn add-weights [weights tree]
+  (let [S (tree-traversal tree)
+        add-edge (fn [node new-edge] (conj node (/ (weights new-edge) 2)))]
+    (second
+     (reduce (fn [[k V] x]
+               (let [nextk [k x]]
+                 [nextk [V (add-edge x nextk)]]))
+             [(first S) (add-edge (first S) (first S))]
+             (reverse (second S))))))
 
 (defn -main [distfn dist-matrix]
   (binding [*M* dist-matrix]
@@ -53,9 +67,11 @@
         (if (or (empty? cluster) (neg? c)) 
           (let [foo (clojure.set/difference
                      (set (flatten (keys dist-matrix)))
-                     (set (flatten (first next-min))))]
-            (distfn (first next-min) (vec foo))
-            [tree [(first next-min) (vec foo)]])
+                     (set (flatten (first next-min))))
+                end-tree [(first next-min) (vec foo)]]
+            
+            [(assoc tree end-tree (apply distfn end-tree))
+             end-tree])
           (recur (dec c) 
                  (min-entry cluster)
                  cluster
@@ -68,4 +84,4 @@
                  (for [i s j s :when (not= i j)] (mapv vector (sort [i j]))))
                 [19 27 8 33 18 13 31 18 36 1 13 
                  26 41 32 29 31 17 14 35 28 12]))]
-  (-main distfn M))
+  (apply add-weights (-main distfn M)))
