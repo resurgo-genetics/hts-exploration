6d115e0e7cf5bd73aaace22fc8702d2f0b0eed1a
Meyerlab <meyerlab@meyerlab.bc.edu>
2014-07-25 22:19:22 -0400

    updates to queries and baum welch algorithm

--- src/hts_exploration/hmm.clj
+++ src/hts_exploration/hmm.clj
@@ -1,14 +1,16 @@
 (ns hts-exploration.hmm
-  (:require [edu.bc.fs :as fs]
-            [clojure.contrib.string :as str]
+  (:require [clojure.contrib.string :as str]
             [clojure.contrib.io :as io]
             [clojure.core.reducers :as r]
-            [clojure.math.numeric-tower :as math])
-  (:use hts-exploration.globals
+            [clojure.math.numeric-tower :as math]
+            [clojure.math.combinatorics :as combo])
+  #_(:use hts-exploration.globals
         edu.bc.utils
         edu.bc.utils.probs-stats
         edu.bc.bio.sequtils.files
         [edu.bc.bio.seq-utils :only (markov-step)]))
+
+(defn sum [coll] (reduce + coll))
 
 (def transitions {:s {:s 0.3 :t 0.7} :t {:s 0.1 :t 0.9}})
 
@@ -41,7 +43,44 @@
   (* (alpha path pos state)
      (beta path pos state)
      (/ (sum
-         (map (fn [j]
-                (* (alpha path j state)
-                   (beta path j state)))
-              (range (count path)))))))
+         (map (fn [statej]
+                (* (alpha path pos statej)
+                   (beta path pos statej)))
+              (keys initial))))))
+
+(defn psi [path pos state1 state2]
+  (let [fun (fn [path pos state1 state2]
+              (* (alpha path pos state1)
+                 (get-in transitions [state1 state2])
+                 (beta path (inc pos) state2)
+                 (get-in emissions [state2 (path (inc pos))])
+                 ))]
+    (/ (fun path pos state1 state2)
+       (sum
+        (for [k (keys initial)
+              l (keys initial)]
+          (fun path pos k l))))))
+
+(reduce (fn [astar [state1 state2]]
+          (update-in astar [state1 state2] (fn [_] (/ (sum 
+                                                      (for [i (range (dec (count path)))]
+                                                        (psi path i state1 state2)))
+                                                     (sum
+                                                      (for [i (range (dec (count path)))]
+                                                        (gamma path i state1)))))))
+        transitions
+        (for [k (keys initial)
+              l (keys initial)]
+          [k l]))
+
+(reduce (fn [bstar [state emit]]
+          (update-in bstar [state emit] (fn [_] (/ (sum 
+                                                   (for [i (range (count path))]
+                                                     (* (if (= (path i) emit) (gamma path i state) 0))))
+                                                  (sum
+                                                   (for [i (range (count path))]
+                                                     (gamma path i state)))))))
+        emissions
+        (for [s (keys initial)
+              e [:A :B]]
+          [s e]))
